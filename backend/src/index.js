a0e16aa4a661382f1e3e8aa147f4fba6a1c52ab3
Omianju <devansh1413@gmail.com>
2025-01-20 15:08:11 +0530

    zustand setup and signup ui integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,16 @@ import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 dotenv.config();
+
+app.use(cors({
+  origin : "http://localhost:5173",
+  credentials : true
+}))
 
 app.use(express.json()); // This will allow us to extract json data from the body
 app.use(cookieParser()); // To parse the value of the cookie
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage"
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import { useAuth } from "./store/useStore";
+import {Loader} from "lucide-react"
 
 const App = () => {
+  const { checkAuth, isCheckingAuth, authUser } = useAuth()
+  console.log(authUser);
+  
+  useEffect(()=>{
+    checkAuth()
+  },[])
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="h-screen w-full flex items-center justify-center">
+        <Loader className="h-6 w-6 animate-spin"/>
+      </div>
+    )
+  }
+
   return (
-    <div className="h-screen w-full ">
-      {/* Open the modal using document.getElementById('ID').showModal() method */}
-      <button
-        className="btn"
-        onClick={() => document.getElementById("my_modal_1").showModal()}
-      >
-        open modal
-      </button>
-      <dialog id="my_modal_1" className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">
-            Press ESC key or click the button below to close
-          </p>
-          <div className="modal-action">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
-            </form>
-          </div>
-        </div>
-      </dialog>
+    <div>
+      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to={"/login"} />}/>
+        <Route path="/signup" element={!authUser ? <SignupPage /> : <Navigate to={"/"}/>}/>
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"}/>}/>
+        <Route path="/settings" element={<SettingsPage />}/>
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to={"/login"} />}/>
+      </Routes>
     </div>
   );
 };
